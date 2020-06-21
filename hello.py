638e50533c3be213832e199951b51dac1eda8903
Pratik Gite <pratikgite2015@gmail.com>
2020-06-21 21:21:27 +0530

    first basics

---
+++ hello.py
@@ -0,0 +1,27 @@
+# Stings
+print("Hello World")
+print("Hello" + " World")
+# end Strings
+
+# Arithemetics Operations
+print('\n')
+print("Addition: " + str(2+3))
+print("Subract: " + str(3-2))
+print("Multiplication: " + str(4*2))
+print("Division: " + str(4/2))
+print("Modulas: " + str(3%2))
+print("Powers: " + str(2**5))
+print("5 times string: " + str("Hello World, " * 5))
+# end Arithemetics Operations
+
+# Logicals
+print('\n')
+# if else
+a = 10
+b = 5
+if a > b:
+	print("Correct: "+str(a)+" is greater then "+str(b))
+else:
+	print("Wrong: "+str(a)+" is not greater then "+str(b))
+# end if else
+# end Logicals
